9c79fb1a6fb472594253b45a4ff29b2a8d384c92
mgall <mgallo185@gmail.com>
2023-10-10 16:41:43 -0400

    finished lab

--- app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
+++ app/src/main/java/com/codepath/articlesearch/ArticleAdapter.kt
@@ -13,7 +13,7 @@ import com.bumptech.glide.Glide
 const val ARTICLE_EXTRA = "ARTICLE_EXTRA"
 private const val TAG = "ArticleAdapter"
 
-class ArticleAdapter(private val context: Context,private val articles: List<Article>) :
+class ArticleAdapter(private val context: Context,private val articles: List<DisplayArticle>) :
     RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -52,9 +52,9 @@ class ArticleAdapter(private val context: Context,private val articles: List<Art
             context.startActivity(intent)
         }
 
-        fun bind(article: Article) {
+        fun bind(article: DisplayArticle) {
 
-            titleTextView.text = article.headline?.main
+            titleTextView.text = article.headline
             abstractTextView.text = article.abstract
 
             Glide.with(context)
